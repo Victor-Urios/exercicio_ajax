0cc58d50401eedf38ab860bd5b7b05616e96cd5b
Victor-Urios <victorurios@hotmail.com>
2025-05-19 21:30:50 -0300

    primeiras impressões do ajax

---
+++ main.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const nameElement = document.querySelector('#name');
+    const usernameElement = document.querySelector('#username');
+    const avatarElement = document.querySelector('#avatar');
+    const reposElement = document.querySelector('#repos');
+    const followersElement = document.querySelector('#followers');
+    const followingElement = document.querySelector('#following');
+    const linkElement = document.querySelector('#link');
+// juntando as ids para colocar as informações dentro ^
+
+//pegando as informações do meu github e atribuindo as informações nos locais designados.
+    fetch('https://api.github.com/users/Victor-Urios')
+    .then(function(res) {
+        return res.json();
+    })
+    .then(function(json) {
+        nameElement.innerText = json.name;
+        usernameElement.innerText = json.login;
+        avatarElement.src = json.avatar_url;
+        followingElement.innerText = json.following;
+        followersElement.innerText = json.followers;
+        reposElement.innerText = json.public_repos;
+        linkElement.href =json.html_url;
+    })
+})
