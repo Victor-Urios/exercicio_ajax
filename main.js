cd2881fd5a5fb6050d4751a730ca06fbff032683
Victor-Urios <victorurios@hotmail.com>
2025-05-29 18:55:18 -0300

    Reescrita

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
-const alunos = ['Gustavo', 'Mariana', 'Paulo', 'Wandersson'];
-//no map a gente pode fazer uma manipulação e ter um novo array como retorno
-const alunos2 =  alunos.map(function(itemAtual) {
-    return { //objeto literal e transformando um array de string em objeto
-        nome: itemAtual, 
-        nota: 6
-    }
-})
+const alunos = [ 
+    { nome:"Joelton", nota: 6 },
+    { nome:"Juliana", nota: 5 },
+    { nome:"Jhonny", nota: 9 },
+    { nome:"Mariana", nota: 3 },
+    { nome:"Waldir", nota: 10}
+]
 
-const alunosPassaram = alunos2.every(function(item) {
-    return item.nota >= 6 //verificação de nota que só permite passar de ano quem tem nota maior que 6
-})
+console.log("Passaram de ano!")
+const passaram = alunos.filter(aluno => aluno.nota >= 6);
+console.log(passaram);
 
-console.log(alunosPassaram);
+console.log("Reprovaram de ano!")
+const reprovaram = alunos.filter(aluno => aluno.nota <= 5);
+console.log(reprovaram);
