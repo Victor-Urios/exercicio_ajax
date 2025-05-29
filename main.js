0b68e8b8d36f97383a4829fb6b592c4918d77f41
Victor-Urios <victorurios@hotmail.com>
2025-05-29 18:18:50 -0300

    é isso?

--- main.js
+++ main.js
@@ -1,25 +1,14 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const nameElement = document.querySelector('#name');
-    const usernameElement = document.querySelector('#username');
-    const avatarElement = document.querySelector('#avatar');
-    const reposElement = document.querySelector('#repos');
-    const followersElement = document.querySelector('#followers');
-    const followingElement = document.querySelector('#following');
-    const linkElement = document.querySelector('#link');
-// juntando as ids para colocar as informações dentro ^
-
-//pegando as informações do meu github e atribuindo as informações nos locais designados.
-    fetch('https://api.github.com/users/Victor-Urios')
-    .then(function(res) {
-        return res.json();
-    })
-    .then(function(json) {
-        nameElement.innerText = json.name;
-        usernameElement.innerText = json.login;
-        avatarElement.src = json.avatar_url;
-        followingElement.innerText = json.following;
-        followersElement.innerText = json.followers;
-        reposElement.innerText = json.public_repos;
-        linkElement.href =json.html_url;
-    })
+const alunos = ['Gustavo', 'Mariana', 'Paulo', 'Wandersson'];
+//no map a gente pode fazer uma manipulação e ter um novo array como retorno
+const alunos2 =  alunos.map(function(itemAtual) {
+    return { //objeto literal e transformando um array de string em objeto
+        nome: itemAtual, 
+        nota: 6
+    }
 })
+
+const alunosPassaram = alunos2.every(function(item) {
+    return item.nota >= 6 //verificação de nota que só permite passar de ano quem tem nota maior que 6
+})
+
+console.log(alunosPassaram);
