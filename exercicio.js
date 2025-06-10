5ba9031f78d8094907944e3e5492ebbb6b5cbec3
Victor-Urios <victorurios@hotmail.com>
2025-06-10 19:19:20 -0300

    Add files via upload

---
+++ exercicio.js
@@ -0,0 +1,15 @@
+"use strict";
+function converteDolar(dolar, saldo) {
+    return dolar * saldo;
+}
+let convertido = converteDolar(5.5, 1250);
+console.log("Foi convertido para sua conta", convertido, "Reais");
+function nome(nome) {
+    return nome;
+}
+let nomee = nome('Vinicius');
+console.log("Olá", nomee, "Gostaria de conhecer nossos serviços?");
+// let dolar = 5.50;
+// let saldo = 1250;
+// let nome = 'Vinicius';
+// console.log(`${nome} tem um saldo de ${dolar * saldo} Dolares!`)
---
+++ exercicio.ts
@@ -0,0 +1,18 @@
+function converteDolar (dolar: number, saldo: number): number {
+    return dolar * saldo;
+}
+let convertido = converteDolar(5.5, 1250);
+console.log("Foi convertido para sua conta", convertido, "Reais")
+
+function nome (nome: string): string { 
+    return nome;
+}
+let nomee = nome('Vinicius')
+
+console.log("Olá", nomee, "Gostaria de conhecer nossos serviços?")
+
+// let dolar = 5.50;
+// let saldo = 1250;
+// let nome = 'Vinicius';
+
+// console.log(`${nome} tem um saldo de ${dolar * saldo} Dolares!`)
